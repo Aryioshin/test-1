9ed7d1aacf019f5e6909b470e619714977b84dde
crypto-cooker <mateo.kova0510@gmail.com>
2024-08-10 06:34:13 +0900

    fix:pic-pos

--- src/components/layout/Decor.tsx
+++ src/components/layout/Decor.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Decor() {
   return (
     <div className="absolute w-full">
-      <div className="relative top-[40px] left-[30%]">
+      <div className="relative top-[40px] left-[calc(50%-375px)] w-fit">
         <div className="relative w-[500px] aspect-video">
           <Image src="/ary2.png" fill alt=""></Image>
         </div>
