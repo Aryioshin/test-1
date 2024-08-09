8ec3a3bbc219547a87aafc7c902cc66ac6a76301
crypto-cooker <mateo.kova0510@gmail.com>
2024-08-10 03:36:23 +0900

    redirect

--- src/app/swap/page.tsx
+++ src/app/swap/page.tsx
@@ -1,11 +1,12 @@
 
 import SwapSide from "@/components/swap/SwapSide"
+import { YourApp } from "@/components/button"
 import { ArrowUpDown, Wallet2 } from "lucide-react"
 
 export default function Page() {
   return (
     <div className="flex justify-center items-center w-full h-[100vh] text-blue-400">
-      <div className="relative w-[500px] h-[400px] shadow-3xl shadow-primary-blue-300 rounded-3xl bg-primary-gray-200 p-3">
+      <div className="relative w-[450px] h-[340px] shadow-3xl shadow-primary-blue-300 rounded-3xl bg-primary-gray-200 p-3">
         <div className="rounded flex justify-center items-center text-2xl text-white font-bold">
           SWAP
         </div>
@@ -14,10 +15,7 @@ export default function Page() {
           <ArrowUpDown className="mx-auto scale-[200%] bg-primary-gray-300 rounded" size={10}/>
           <SwapSide />
         </div>
-        <div className="flex items-center h-[50px] rounded-2xl my-2 bg-primary-gray-300 p-8">
-          Fees
-        </div>
-        <div>
+        <div className="my-4">
           <button className="flex w-full bg-primary-gray-100 rounded-3xl justify-center text-2xl p-4">
             <Wallet2 />
             Connect Wallet
