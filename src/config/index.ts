27c6f0af4c1a5e6833337079024684833f16fd90
crypto-cooker <mateo.kova0510@gmail.com>
2024-08-11 00:04:58 +0900

    fix:cronosConfig

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,6 +1,6 @@
 export const TOKEN_LIST = [
-  { name: "ARY", address: "0x41bc026dABe978bc2FAfeA1850456511ca4B01bc" },
   { name: "CRO", address: "" },
+  { name: "ARY", address: "0x41bc026dABe978bc2FAfeA1850456511ca4B01bc" },
   { name: "WCRO", address: "0xA0b73E1Ff0B80914AB6fe0444E65848C4C34450b" },
   { name: "USDC", address: "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48" },
   { name: "USDT", address: "0xdAC17F958D2ee523a2206206994597C13D831ec7" }
@@ -20,12 +20,10 @@ export const cronosConfig = {
     name: 'Cronos',
     symbol: 'CRO',
   },
-  rpcUrls: {
-    public: { http: ['https://cronos.blockpi.network/v1/rpc/public'] },
-    default: { http: ['https://cronos.blockpi.network/v1/rpc/public'] },
-  },
-  blockExplorerUrls: {
-    etherscan: { name: 'CronoScan', url: 'https://cronoscan.com' },
-    default: { name: 'CronoScan', url: 'https://cronoscan.com' },
-  }
+  rpcUrls: [
+    'https://cronos.blockpi.network/v1/rpc/public'
+  ],
+  blockExplorerUrls: [
+    'https://cronoscan.com'
+  ]
 }
