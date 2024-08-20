733f7ec7a883e04f5a8b607df30fa2b80291c011
crypto-cooker <mateo.kova0510@gmail.com>
2024-08-21 06:47:04 +0900

    bugfixed

--- src/components/competition/PrizeTable.tsx
+++ src/components/competition/PrizeTable.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 export default function PrizeTable() {
   return (
-    <div className="relative px-4 py-9 rounded-2xl my-4">
+    <div className="relative px-4 py-4 rounded-2xl my-4">
       <div className="absolute aspect-square top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 md:w-1/2 h-full z-[0] opacity-30">
         <Image src="/prize.png" fill alt=""/>
       </div>
@@ -20,7 +20,7 @@ export default function PrizeTable() {
             REWARD
           </div>
         </div>
-        <div className="flex justify-center gap-8 my-8">
+        <div className="flex justify-center gap-8 my-6">
           <div className="relative text-3xl w-1/3">
             <div className="absolute top-1/2 left-1/2 rotate-12 -translate-x-1/2 -translate-y-1/2 aspect-[224/416] w-[30px]">
               <Image src="/medal1.png" fill alt="" />
@@ -30,7 +30,7 @@ export default function PrizeTable() {
             120$
           </div>
         </div>
-        <div className="flex justify-center gap-8 my-8">
+        <div className="flex justify-center gap-8 my-6">
           <div className="relative text-3xl w-1/3">
             <div className="absolute top-1/2 left-1/2 rotate-12 -translate-x-1/2 -translate-y-1/2 aspect-[224/416] w-[30px]">
               <Image src="/medal2.png" fill alt="" />
@@ -40,7 +40,7 @@ export default function PrizeTable() {
             80$
           </div>
         </div>
-        <div className="flex justify-center gap-8 my-8">
+        <div className="flex justify-center gap-8 my-6">
           <div className="relative text-3xl w-1/3">
             <div className="absolute top-1/2 left-1/2 rotate-12 -translate-x-1/2 -translate-y-1/2 aspect-[224/416] w-[30px]">
               <Image src="/medal3.png" fill alt="" />
