19b270f82113324fe868e8894940cfd38b5fe6d9
crypto-cooker <mateo.kova0510@gmail.com>
2024-08-22 02:20:34 +0900

    fix:initial token

--- src/config/wagmi.ts
+++ src/config/wagmi.ts
@@ -1,11 +1,6 @@
 
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
 import {
-  arbitrum,
-  base,
-  mainnet,
-  optimism,
-  polygon,
   sepolia,
   cronos,
 } from 'wagmi/chains';
@@ -14,11 +9,6 @@ export const config = getDefaultConfig({
   appName: 'RainbowKit text',
   projectId: 'YOUR_PROJECT_ID',
   chains: [
-    mainnet,
-    polygon,
-    optimism,
-    arbitrum,
-    base,
     cronos,
     sepolia,
     ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === 'true' ? [sepolia] : []),
